a67c8abe2cf94b62132fbac9604ac5db575fb599
GangsangMSK <wjdgudtk1234@gmail.com>
2022-09-16 03:53:38 +0900

    Add:BOJ 1152

---
+++ SwiftAlgorithm/Class1/1152.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 김민석 on 2022/09/16.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator:" ").map { String($0) }
+print(input.count)
