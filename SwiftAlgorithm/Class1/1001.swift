97adbce927b9a41cbe25bb4e64652918be1e3dd5
GangsangMSK <wjdgudtk1234@gmail.com>
2022-09-16 03:18:26 +0900

    Add:BOJ 1001

---
+++ SwiftAlgorithm/Class1/1001.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 김민석 on 2022/09/16.
+//
+
+import Foundation
+
+import Foundation
+
+let input = readLine()!.split(separator:" ").map { Int(String($0))! }
+print(input[0]-input[1])
