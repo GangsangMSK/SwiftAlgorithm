616330c7d3682e48d002e5b524c39a3f825f3557
GangsangMSK <wjdgudtk1234@gmail.com>
2022-09-16 03:27:38 +0900

    Add:BOJ 1008

---
+++ SwiftAlgorithm/Class1/1008.swift
@@ -0,0 +1,10 @@
+//
+//  1008.swift
+//  SwiftAlgorithm
+//
+//  Created by 김민석 on 2022/09/16.
+//
+import Foundation
+
+let input = readLine()!.split(separator:" ").map { Double(String($0))! }
+print(input[0]/input[1])
