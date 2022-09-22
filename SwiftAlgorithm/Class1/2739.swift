6d14f59351004483bb764df0e9b821aaf3d1a8bd
GangsangMSK <wjdgudtk1234@gmail.com>
2022-09-23 00:13:20 +0900

    Add:BOJ 2739

---
+++ SwiftAlgorithm/Class1/2739.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 김민석 on 2022/09/21.
+//
+
+import Foundation
+
+let input = Int(readLine()!)
+
+for i in 1...9 {
+    print("\(input!) * \(i) =",input! * i)
+}
