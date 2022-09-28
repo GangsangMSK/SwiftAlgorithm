ac9aa38412edff1acfcfbfb57585b137b6dcd103
GangsangMSK <wjdgudtk1234@gmail.com>
2022-09-28 14:33:13 +0900

    Add:BOJ 10171

---
+++ SwiftAlgorithm/Class1/10171.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 김민석 on 2022/09/28.
+//
+
+import Foundation
+
+let cat = #"\    /\"# + "\n" +  #" )  ( ')"# + "\n" + #"(  /  )"# + "\n" + #" \(__)|"#
+
+print(cat)
