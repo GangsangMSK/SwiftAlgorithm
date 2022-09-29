36af76dab111877589ee3bb90a7d78e732aaeb39
GangsangMSK <wjdgudtk1234@gmail.com>
2022-09-29 21:18:29 +0900

    Add:BOJ 10172 개

---
+++ SwiftAlgorithm/Class1/10172.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 김민석 on 2022/09/29.
+//
+
+import Foundation
+
+let dog = #"|\_/|"# + "\n" + #"|q p|   /}"# + "\n" + #"( 0 )"""\"# + "\n" + #"|"^"`    |"# + "\n" +  #"||_/=\\__|"#
+print(dog)
