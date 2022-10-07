a5881e2a0691894e6ecba599061402268f83ab6f
GangsangMSK <wjdgudtk1234@gmail.com>
2022-10-07 22:28:15 +0900

    Add:BOJ 10998 AxB

---
+++ SwiftAlgorithm/Class1/10998.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 김민석 on 2022/10/07.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+
+print(input[0]*input[1])
